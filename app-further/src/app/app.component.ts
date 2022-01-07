863f58d876e96578ab9a0d1f635625bce1ff7fd3
kmcolina <kathymcolina@gmail.com>
2022-01-07 15:18:04 -0400

    viewchild using directives

--- app-further/src/app/app.component.ts
+++ app-further/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild, } from '@angular/core';
 import { ChildComponent } from './child.component';
+import { SharkDirective } from './shark.directive';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,33 @@ import { ChildComponent } from './child.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements AfterViewInit{
+
+  //pipe
   title = 'app-further Hola mundo';
+
+  //view child change dom
   @ViewChild('info')
   info!: ElementRef;
 
+  //viewchild use secundary component
   @ViewChild(ChildComponent)
   child!: ChildComponent;
+
+  //view child use directive
+
+  extraCreature!: string;
+
+  @ViewChild(SharkDirective)
+  set appShark(directive: SharkDirective) {
+    this.extraCreature = directive.creature;
+  };
 
 
   ngAfterViewInit() {
     console.log(this.info.nativeElement.innerHTML);
     this.info.nativeElement.innerHTML = "DOM updated succesfully!!!";
     console.log(this.child.whoAmI()); // I am a child component!
+    console.log(this.extraCreature);
   }
 }
 
--- app-further/src/app/app.module.ts
+++ app-further/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiltroPalabraPipePipe } from './filtro-palabra-pipe.pipe';
 import { ChildComponent } from './child.component';
+import { SharkDirective } from './shark.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     FiltroPalabraPipePipe,
-    ChildComponent
+    ChildComponent,
+    SharkDirective
   ],
   imports: [
     BrowserModule,
