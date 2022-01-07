5fdc5b9463e4bad19ff1fc3e2cf5097d83d2793b
kmcolina <kathymcolina@gmail.com>
2022-01-07 14:53:26 -0400

    viewchild DOM

--- app-further/src/app/app.component.ts
+++ app-further/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements AfterViewInit{
   title = 'app-further Hola mundo';
+  @ViewChild('info')
+  info!: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.info.nativeElement.innerHTML);
+    this.info.nativeElement.innerHTML = "DOM updated succesfully!!!";
+  }
 }
+
